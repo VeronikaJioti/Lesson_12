14dacefd38118e217dc19e1101b2b2494372ebb7
VeronikaJioti <v.jioeva@gmail.com>
2022-04-19 12:47:29 +0400

    Upd

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,24 @@ const sass = require('gulp-sass')(require('sass'));
 
 sass.compiler = require('node-sass');
 
-function buildStyles() {
-  return gulp.src('scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('styles'));
-};
+// function buildStyles() {
+//   return gulp.src('scss/style.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('css'));
+// };
+
+// exports.buildStyles = buildStyles;
+// exports.watch = function () {
+//   gulp.watch('scss/style.scss', ['sass']);
+// };
 
 gulp.task('sass', function() {
     return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError)) 
-    .pipe(gulp.dest('styles'));  
+    .pipe(gulp.dest('css'));  
 });
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('scss/style.scss', ['sass']);
-};
+gulp.task('watch', function () {
+    gulp.watch('scss/style.scss', gulp.parallel('sass'));
+  });
+
